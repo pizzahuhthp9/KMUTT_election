0c753c8b42851d5bc14f59d10d229c14229aa02e
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-04-03 13:41:34 +0700

    create progress component

---
+++ src/components/ProgressHead.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="h-1 w-screen mt-8 px-5 mb-3 border-black border-b-3 flex justify-around items-center">
+          
+          
+          <div class="h-5 w-5 bg-black rounded-full border-black border-3" v-for="index in parseInt(passed)" :key="index"></div>
+          <div class="h-5 w-5 bg-main rounded-full border-black border-3" v-for="index in parseInt(amount-passed)" :key="index"></div>
+          <!-- <div class="h-5 w-5 bg-main rounded-full border-black border-3" ></div>
+          <div class="h-5 w-5 bg-main rounded-full border-black border-3" ></div>
+          <div class="h-5 w-5 bg-main rounded-full border-black border-3" ></div>
+          <div class="h-5 w-5 bg-main rounded-full border-black border-3" ></div> -->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        amount: Number,
+        passed: Number
+    }
+}
+</script>
+
+<style>
+
+</style>
