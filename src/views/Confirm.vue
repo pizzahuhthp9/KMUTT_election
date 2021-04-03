4baf14ef06a91042d979c8155fcc7ee14357124f
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-04-03 13:42:01 +0700

    create form page

---
+++ src/views/Confirm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+    name: "Confirm"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Form.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <div id="header">
+          <progress-head amount="5" passed="1"></progress-head>
+      </div>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ProgressHead from '@/components/ProgressHead.vue';
+export default {
+    name: "Form",
+    components:{
+        ProgressHead
+    }
+}
+</script>
+
+        ProgressHead
+<style>
+
+</style>
